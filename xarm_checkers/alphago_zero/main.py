697e62eefacf19cda1dee37b87862134c23c5cc0
matthew-gries <gries.m@husky.neu.edu>
2022-04-19 00:18:20 -0400

    AHHHHHHHHHHHHHHH

--- xarm_checkers/alphago_zero/main.py
+++ xarm_checkers/alphago_zero/main.py
@@ -3,8 +3,10 @@ import logging
 import coloredlogs
 
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
+# from othello.OthelloGame import OthelloGame as Game
+# from othello.pytorch.NNet import NNetWrapper as nn
+from xarm_checkers.learning.checkers import CheckersGame as Game
+from xarm_checkers.learning.checkers_nn_wrapper import CheckersNNWrapper as nn
 from utils import *
 
 log = logging.getLogger(__name__)
@@ -31,7 +33,8 @@ args = dotdict({
 
 def main():
     log.info('Loading %s...', Game.__name__)
-    g = Game(6)
+    # g = Game(6)
+    g = Game()
 
     log.info('Loading %s...', nn.__name__)
     nnet = nn(g)
